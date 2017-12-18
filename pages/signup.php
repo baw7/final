94612f8aa074cf346188b2eb3104f727df747592
baw7 <31857911+baw7@users.noreply.github.com>
2017-12-17 20:25:08 -0500

    Create signup.php

---
+++ pages/signup.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html>
+<body>
+
+
+<form action="index.php?page=accounts&action=userSignUp" method="post">
+    First name: <input type="text" name="fname"><br>
+    Last name: <input type="text" name="lname"><br>
+    Email: <input type="text" name="email"><br>
+    Phone: <input type="text" name="phone"><br>
+    Birthday: <input type="text" name="birthday"><br>
+    Gender: <input type="text" name="gender"><br>
+    Password: <input type="password" name="password"><br>
+    <input type="submit" value="Submit form">
+</form>
+
+
+</body>
+</html>
