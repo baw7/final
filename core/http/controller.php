19733322071560c1a712d6655b33ed9288795673
baw7 <31857911+baw7@users.noreply.github.com>
2017-12-17 19:48:08 -0500

    Create controller.php

---
+++ core/http/controller.php
@@ -0,0 +1,10 @@
+<?php
+namespace http;
+class controller
+{
+    static public function getTemplate($template, $data = NULL)
+    {
+        $template = 'pages/' . $template . '.php';
+        include $template;
+    }
+}
