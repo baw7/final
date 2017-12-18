ebe22b7c7f633e732c43e31b610a57becc30123c
baw7 <31857911+baw7@users.noreply.github.com>
2017-12-18 16:31:37 -0500

    Update register.php

--- pages/register.php
+++ pages/register.php
@@ -10,7 +10,8 @@
     Email: <input type="text" name="email"><br>
     Phone: <input type="text" name="phone"><br>
     Birthday: <input type="text" name="birthday"><br>
-    Gender: <input type="text" name="gender"><br>
+    Gender:  <input type="radio" name="gender" value="male" checked> Male<br>
+       <input type="radio" name="gender" value="female"> Female<br>
     Password: <input type="password" name="password"><br>
     <input type="submit" value="Submit form">
 </form>
