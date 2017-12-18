ed18a4267ac61518bd21125dec10ee622f7798f0
baw7 <31857911+baw7@users.noreply.github.com>
2017-12-17 20:27:53 -0500

    Update homepage.php

--- controllers/homepage.php
+++ controllers/homepage.php
@@ -1,1 +1,14 @@
-
+<?php
+class homepage extends http\controller
+{
+    public static function show()
+    {
+        $templateData['site_name'] = 'Brianna Wong - IS 601 Final Project';
+        self::getTemplate('homepage', $templateData);
+    }
+    public static function create()
+    {
+        print_r($_POST);
+    }
+}
+?>
