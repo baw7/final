32ddf6fa388a850d8bbfe11b8e5afbd215586f7f
baw7 <31857911+baw7@users.noreply.github.com>
2017-12-18 11:54:38 -0500

    Update homepage.php

--- pages/homepage.php
+++ pages/homepage.php
@@ -8,16 +8,16 @@
     <?php
     echo $data['site_name'];
     ?> </h2>
-
-<h3><a href="index.php?page=accounts&action=all">Show All Accounts</a></h3>
-<h3><a href="index.php?page=tasks&action=all">Show All Tasks</a></h3>
+<center>
+<h3><a href="index.php?page=accounts&action=all"> All Accounts</a></h3>
+<h3><a href="index.php?page=tasks&action=all"> All Tasks</a></h3>
 
 <form action="index.php?page=accounts&action=login" method="POST">
 
     <div class="container">
         <label><b>Username</b></label>
         <input type="text" placeholder="Enter Username" name="email" required>
-
+<br>
         <label><b>Password</b></label>
         <input type="password" placeholder="Enter Password" name="pass" required>
         <br>
@@ -29,7 +29,7 @@
 
 
 </form>
-<h3><a href="index.php?page=accounts&action=signup">New User?</a></h3>
+<h3><a href="index.php?page=accounts&action=signup">New? Register Here</a></h3>
 
 
 </body>
