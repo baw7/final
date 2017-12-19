bc01fdc8f95185375765c0ecf3973b43113a4b2d
baw7 <31857911+baw7@users.noreply.github.com>
2017-12-18 21:47:55 -0500

    Update accountsController.php

--- controllers/accountsController.php
+++ controllers/accountsController.php
@@ -64,7 +64,7 @@ $user = accounts::findUserbyEmail($_REQUEST['email']);
     }
     public static function login()
      {
-        $user = useraccounts::findUname($_REQUEST['username']);
+        $user = accounts::findUname($_REQUEST['username']);
         if ($user == FALSE) {
 			echo "user not found!";
             
