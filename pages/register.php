01334d9b9db9bb04665825683051cb7e5a641598
baw7 <31857911+baw7@users.noreply.github.com>
2017-12-18 19:50:31 -0500

    Update register.php

--- pages/register.php
+++ pages/register.php
@@ -3,17 +3,11 @@
 <html>
 <body>
 
-<<<<<<< HEAD
 <center>
-<form action="index.php?page=accounts&action=register" method="post">
-    First Name: <input type="text" name="fname"><br>
-    Last Name: <input type="text" name="lname"><br>
-=======
 
 <form action="index.php?page=accounts&action=register" method="post">
     First name: <input type="text" name="fname"><br>
     Last name: <input type="text" name="lname"><br>
->>>>>>> 9f96c684917892afce31c7dfe181be2a7754b3bb
     Email: <input type="text" name="email"><br>
     Phone: <input type="text" name="phone"><br>
     Birthday: <input type="text" name="birthday"><br>
