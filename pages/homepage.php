9b463f8656716504c0de0fe64952a4a36fac09c2
baw7 <31857911+baw7@users.noreply.github.com>
2017-12-18 22:46:13 -0500

    Update homepage.php

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,7 +1,6 @@
 <!doctype html>
 <head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-</head>
+   <link rel="stylesheet" href="main.css"></head>
 <html>
 <body>
 
