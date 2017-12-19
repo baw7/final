32c918c4b2c84569211008bc56fa5dbf8170512f
baw7 <31857911+baw7@users.noreply.github.com>
2017-12-18 20:38:46 -0500

    Update tasks.php

--- pages/tasks.php
+++ pages/tasks.php
@@ -1,28 +1,21 @@
-<!DOCTYPE html>
-
-<html>
-<body>
-<div class="search-box">
-<button class="dismiss"><i class="icon-close"></i></button>
-<form id="searchForm" action="#" role="search">
-<input type="search" placeholder="Search" class="form-control">
-</form>
-</div>
-<p><li class="nav-item">
-<form action="index.php?page=accounts&action=logout" method="post" id="form3" style="float:right;">
-<li><button class="btn btn-link navbar-btn" type="submit">Logout</button></li>
-</form>
-<p><form action="index.php?page=accounts&action=myProf" method="post" id="form4" style="float:right;">
-<li><span class="glyphicon glyphicon-user"></span><button class="btn btn-link navbar-btn" type="submit">My Profile</button>
-</li>
-</form> 
-<p><ul class="list-unstyled">
-<form action="index.php?page=tasks&action=create" method="POST" style="float:left;">
-<button class="btn btn-link navbar-btn" class="icon-interface-windows" type="submit"></i>Create Task</button>
-</form></ul>
-
-         <?php
-print utility\htmlTable::genarateTableFromMultiArray($data);
+<?php
+session_start();
+print table\htmlTable::generateTableForOneTodo($data);
 ?>
+<html>
+<head>
+<style>
+body {
+}
+</style>
+</head>
+<body>
+<form action="index.php?page=tasks&action=edit&id=<?php echo $data->id; ?> " method="post" id="form1" style="float:left;">
+<button class="btn btn-primary text-center" type="submit" form="form1" value="edit">Edit</button></div>
+</form>
+<form action="index.php?page=tasks&action=delete&id=<?php echo $data->id; ?> " method="post" id="form2" style="float:right;">
+<button class="btn btn-primary" type="submit" form="form2" value="delete">Delete</button>
+<a href="index.php?page=tasks&action=allOneUser&id=<?php echo $_SESSION["userID"] ?>">Cancel</a></div>
+</form>
 </html>
 </body>
