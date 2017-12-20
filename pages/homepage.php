9363a49a8282f1b22a7cd9c379181525ff7bfd78
baw7 <31857911+baw7@users.noreply.github.com>
2017-12-20 10:53:59 -0500

    Update homepage.php

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,6 +1,88 @@
 <!doctype html>
 <head>
-   <link rel="stylesheet" href="main.css"></head>
+  nav { 
+	 margin-top:25px;
+	float:left;
+} 
+
+body
+{
+background-color: #EBF5FB;
+font-family: verdana;
+font-size:9px;
+}
+
+h3
+{
+font-family: verdana;
+font-size:9px;
+}
+
+h4
+{
+font-family: verdana;
+font-size:9px;
+}
+
+h1
+{
+font-family: verdana;
+font-size:11px;
+}
+
+h2
+{
+font-family: verdana;
+font-size:11px;
+}
+	
+header .row {
+padding: 10px 25px 25px 10px;
+font-weight:bold;
+}
+
+td, th {	padding: 6px; }
+
+tr {
+    font-size: 12px;
+	border:none;
+}
+tr th { border-bottom:#ccc 1px solid;  }
+tr:nth-child(odd) {
+    background-color: white;
+}
+
+button.edit {
+    padding: 6px 12px 6px 12px;
+    background-color: grey;
+    color: grey;
+}
+
+button.edit:hover {
+    padding: 6px 12px 6px 12px;
+    background-color: grey;
+    color: white;
+}
+button.delete:hover {
+    background-color: yellow;
+    color: white;
+}
+#contact {
+	padding:35px;
+	background-color:white;
+}
+footer {
+	margin-top:75px;
+	
+}
+
+div#field {
+    margin-bottom: 10px;
+}
+#updateForm input {
+    margin-bottom: 18px;
+}
+</head>
 <html>
 <body>
 
